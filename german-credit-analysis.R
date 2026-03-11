bd4c12464294f7e39d76d187222e15f020350583
joantheexplorer <contactjkeaviles@gmail.com>
2026-03-12 00:07:29 +0800

    Staging pre-requisites

---
+++ german-credit-analysis.R
@@ -0,0 +1,16 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ---- STATS 203: Statistics and Probability for CS/IT Students ----
+# ---- Members: Acob, Alapag, Artates, Aviles, Pauso (BSCS 2-4) ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ---- Load Needed Libraries ----
+library(readxl)
+library(dplyr)
+library(gglot2)
+
+# ---- 1. Custom Functions ----
+# ---- 2. Data Structures ----
+# ---- 3. Data Manipulations ----
+# ---- 4. Data Visualization ----
+# ---- End of Program ----
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
